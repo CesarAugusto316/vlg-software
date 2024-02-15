347f95e1aff8d26b515a9d59257a8b2c04833969
cesaraugusto316 <cesaraugusto316@protonmail.com>
2024-02-14 21:05:38 -0500

    ✨ Update HTML and package.json, add flowbite and zustand dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import './index.css';
 import { router } from './router.tsx';
+import './index.css';
+import 'flowbite';
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
