cd499fd0fcf2580e0d35c4e9e348c69f9e7ea14a
cesaraugusto316 <cesaraugusto316@protonmail.com>
2024-02-16 15:07:13 -0500

    ✨ Refactor login form and update styles

--- src/app/login/Login.tsx
+++ src/app/login/Login.tsx
@@ -1,29 +1,47 @@
 import { FC } from 'react';
+import vlgLogo from '../../assets/icons/vlg-logo.png';
 
 
 export const Login: FC = () => {
   return (
     <div className="bg-gradient-to-b from-gray-vlg-200 to-white h-screen to-90% flex items-center justify-center">
 
-      <form className="w-[400px] bg-white p-8 rounded-xl border-gray-vlg-100 border-2">
+      <form className="-mt-10 w-[460px] bg-white p-8 rounded-xl border-gray-vlg-100 border-2">
+        <ul className="flex flex-col gap-5">
+          <li className="flex items-center gap-2">
+            <img
+              className="w-12 h-12 shadow-md-light"
+              src={vlgLogo}
+              alt="vlg-logo"
+            />
+            <h2 className="text-blue-vlg-900 ">VLG Software</h2>
+          </li>
 
-        <div className="mb-5">
-          <label htmlFor="email" className="block mb-2 text-sm font-medium  dark:text-white">Your email</label>
-          <input
-            type="email"
-            id="email"
-            className="shadow-sm  bg-white border border-gray-vlg-300 font-medium text-sm rounded-lg focus:ring-blue-vlg-900 focus:border-blue-vlg-900 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:shadow-sm-light placeholder:italic placeholder-gray-vlg-400/80"
-            placeholder="name@flowbite.com"
-            required
-          />
-        </div>
+          <li>
+            <h2>Bienvenido!</h2>
+            <p>No tienes cuenta VLG? <a href="#" >Crea una ahora</a></p>
+          </li>
 
-        <button
-          type="submit"
-          className="text-white bg-blue-vlg-500 hover:bg-blue-vlg-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
-        >
-          Register new account
-        </button>
+          <li>
+            <label htmlFor="email" className="block mb-2 text-sm font-medium  dark:text-white">Your email</label>
+            <input
+              type="email"
+              id="email"
+              className="shadow-sm  bg-white border border-gray-vlg-300 font-medium text-sm rounded-lg focus:ring-blue-vlg-900 focus:border-blue-vlg-900 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:shadow-sm-light placeholder:italic placeholder-gray-vlg-400/80"
+              placeholder="name@flowbite.com"
+              required
+            />
+          </li>
+
+          <li>
+            <button
+              type="submit"
+              className="text-white bg-blue-vlg-500 hover:bg-blue-vlg-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            >
+              Register new account
+            </button>
+          </li>
+        </ul>
 
       </form>
     </div>
