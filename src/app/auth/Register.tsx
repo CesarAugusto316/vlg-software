86511aef8c697f3426aa70e9cbbaccbf3a471bb3
cesaraugusto316 <cesaraugusto316@protonmail.com>
2024-02-16 20:26:07 -0500

    ✨ Refactor Register component to use Formik and add validation

--- src/app/auth/Register.tsx
+++ src/app/auth/Register.tsx
@@ -1,108 +1,135 @@
 import { FC } from 'react';
 import { AuthWrapper } from './components/Section';
 import { LogoTitle } from './components/LogoTitle';
-import { Link } from 'react-router-dom';
+import { Form, Link } from 'react-router-dom';
+import { FieldWithErrorMessage } from '../../components/FieldWithErrorMessage';
+import { Formik } from 'formik';
+import * as Yup from 'yup';
+
+
+type FormValues = {
+  name: string;
+  lastName: string;
+  ruc: string;
+  email: string;
+  password: string;
+  rePassword: string;
+}
+
+const validationSchema = Yup.object<FormValues>({
+  name: Yup.string().required('Ingresa tus nombres'),
+  lastName: Yup.string().required('Ingresa tus apellidos'),
+  ruc: Yup.string().required('Ingresa tu RUT'),
+  email: Yup.string().email('Ingresa un correo válido').required('Ingresa tu correo'),
+  password: Yup.string().min(6).required('Ingresa tu contraseña'),
+  rePassword: Yup.string().oneOf([Yup.ref('password'), ''], 'Las contraseñas no coinciden').required('Repite tu contraseña'),
+});
 
 
 export const Register: FC = () => {
   return (
     <AuthWrapper>
 
-      <form className=" form-container -mt-4 w-[680px] flex flex-col gap-4">
-        <ul className="flex flex-col gap-4">
-          <li>
-            <LogoTitle />
-          </li>
+      <Formik
+        validationSchema={validationSchema}
+        initialValues={{
+          name: '',
+          lastName: '',
+          ruc: '',
+          email: '',
+          password: '',
+          rePassword: ''
+        }}
+        onSubmit={(values) => {
+          console.log(values);
+        }}>
+        {() => (
+          <Form className=" form-container -mt-4 w-[680px] flex flex-col gap-4">
+            <ul className="flex flex-col gap-4">
+              <li>
+                <LogoTitle />
+              </li>
 
-          <li>
-            <h2>Crea tu cuenta</h2>
-            <p>Ingresa tus datos para comenzar</p>
-          </li>
-        </ul>
+              <li>
+                <h2>Crea tu cuenta</h2>
+                <p>Ingresa tus datos para comenzar</p>
+              </li>
+            </ul>
 
-        <ul className="grid grid-cols-2 gap-x-8 gap-y-4">
+            <ul className="grid grid-cols-2 gap-x-8 gap-y-4">
 
-          <li>
-            <label htmlFor="name" className="input-label">Nombres</label>
-            <input
-              type="text"
-              id="name"
-              className="input-primary"
-              placeholder="Ingresa tus nombres"
-              required
-            />
-          </li>
+              <li>
+                <FieldWithErrorMessage
+                  name="name"
+                  type="text"
+                  placeholder="Ingresa tus nombres"
+                  label="Nombres"
+                />
+              </li>
 
-          <li>
-            <label htmlFor="lastname" className="input-label">Apellidos</label>
-            <input
-              type="text"
-              id="lastname"
-              className="input-primary"
-              placeholder="Ingresa tus apellidos"
-              required
-            />
-          </li>
+              <li>
+                <FieldWithErrorMessage
+                  name="lastName"
+                  type="text"
+                  placeholder="Ingresa tus apellidos"
+                  label="Apellidos"
+                />
+              </li>
 
-          <li>
-            <label htmlFor="rut" className="input-label">Rut</label>
-            <input
-              type="text"
-              id="rut"
-              className="input-primary"
-              placeholder="Sin puntos ni guión"
-              required
-            />
-          </li>
+              <li>
+                <FieldWithErrorMessage
+                  name="ruc"
+                  type="text"
+                  placeholder="Sin puntos ni guión"
+                  label="RUT"
+                />
+              </li>
 
-          <li>
-            <label htmlFor="email" className="input-label">Correo</label>
-            <input
-              type="email"
-              id="email"
-              className="input-primary"
-              placeholder="Ingresa tu correo"
-              required
-            />
-          </li>
+              <li>
+                <FieldWithErrorMessage
+                  name="email"
+                  type="email"
+                  placeholder="Ingresa tu correo"
+                  label="Correo"
+                />
+              </li>
 
-          <li>
-            <label htmlFor="password" className="input-label">Contraseña</label>
-            <input
-              type="password"
-              id="password"
-              className="input-primary"
-              placeholder="Ingresa tu contraseña"
-              required
-            />
-          </li>
+              <li>
+                <FieldWithErrorMessage
+                  name="password"
+                  type="password"
+                  placeholder="Ingresa tu contraseña"
+                  label="Contraseña"
+                />
+              </li>
 
-          <li>
-            <label htmlFor="re-password" className="input-label">Confirmar contraseña</label>
-            <input
-              type="password"
-              id="re-password"
-              className="input-primary"
-              placeholder="Repite tu contraseña"
-              required
-            />
-          </li>
-        </ul>
+              <li>
+                <FieldWithErrorMessage
+                  name="rePassword"
+                  type="password"
+                  placeholder="Repite tu contraseña"
+                  label="Confirmar contraseña"
+                />
+              </li>
+            </ul>
 
-        <ul className="flex flex-col gap-2 mt-5 w-2/3 mx-auto">
-          <li>
-            <button
-              className="btn-primary"
-            >
-              Continuar
-            </button>
-          </li>
+            <ul className="flex flex-col gap-2 mt-5 w-2/3 mx-auto">
+              <li>
+                <button
+                  type="submit"
+                  className="btn-primary"
+                >
+                  Continuar
+                </button>
+              </li>
 
-          <li>
-            <Link className="btn-light-no-border" to="/login">Volver al Login</Link>
-          </li>
-        </ul>
-      </form>
+              <li>
+                <Link className="btn-light-no-border" to="/login">Volver al Login</Link>
+              </li>
+            </ul>
+          </Form>
+        )}
+      </Formik>
     </AuthWrapper>
   );
 };
