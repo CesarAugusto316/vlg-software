ed3f13daf4781c9faae551287e08e444e3cff1e0
cesaraugusto316 <cesaraugusto316@protonmail.com>
2024-02-18 12:46:46 -0500

    ✨ Update react-router-dom import in AuthContainer component

--- src/app/auth/components/AuthContainer.tsx
+++ src/app/auth/components/AuthContainer.tsx
@@ -1,5 +1,5 @@
 import { FC, ReactNode, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Navigate } from 'react-router-dom';
 import { useVlgStore } from '../../../vlgStore/vlgStore';
 import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from '../../../firebase/firebaseConfig';
@@ -13,13 +13,8 @@ export const AuthContainer: FC<AuthWrapperProps> = ({ children }) => {
   const accessToken = useVlgStore(state => state.accountProfile?.accessToken);
   const setAccessToken = useVlgStore(state => state.setAccountProfile);
 
-  const navigate = useNavigate();
-
   useEffect(() => {
-    if (accessToken && window.location.pathname === '/login') {
-      navigate('/');
-      return;
-    }
+    if (accessToken) return;
     const unsubscribe = onAuthStateChanged(auth, async (user) => {
       if (user) {
         const accessToken = await user.getIdToken();
@@ -29,6 +24,11 @@ export const AuthContainer: FC<AuthWrapperProps> = ({ children }) => {
 
     return () => unsubscribe && unsubscribe();
   }, [accessToken]);
+
+
+  if (accessToken) {
+    return <Navigate to="/" />;
+  }
 
   return (
     <section className="bg-gradient-to-b from-gray-vlg-200 to-white w-screen h-screen overflow-hidden to-90% flex items-center justify-center">
