47837916b811885194a5c4dfc325b2dc078d9839
cesaraugusto316 <cesaraugusto316@protonmail.com>
2024-02-18 09:24:45 -0500

    ✨ Refactor Register component transitions

--- src/app/auth/register/Register.tsx
+++ src/app/auth/register/Register.tsx
@@ -8,21 +8,22 @@ import { slidesList } from './SlidesList';
 export const Register: FC = () => {
   const index = useSlides(state => state.index);
   const direction = useSlides(state => state.direction);
-  const firstRender = useSlides(state => state.firstRender);
-  const setFirstRender = useSlides(state => state.setFirstRender);
+  // const firstRender = useSlides(state => state.firstRender);
+  // const setFirstRender = useSlides(state => state.setFirstRender);
 
   const transRef = useSpringRef();
   const transitions = useTransition(index, {
     ref: transRef,
     keys: null,
-    from: firstRender ? { opacity: 1, transform: 'translate3d(0%,0,0)' } : { opacity: 0, transform: `translate3d(${100 * direction}%,0,0)` }, // Use firstRender in from
+    from: { opacity: 0, transform: `translate3d(${100 * direction}%,0,0)` },
+    // from: firstRender ? { opacity: 1, transform: 'translate3d(0%,0,0)' } : { opacity: 0, transform: `translate3d(${100 * direction}%,0,0)` }, // Use firstRender in from
     enter: { opacity: 1, transform: 'translate3d(0%,0,0)' },
   });
 
   useEffect(() => {
     transRef.start();
-    setFirstRender(); // Set firstRender to false after first render
-  }, [index, transRef, setFirstRender]);
+    // setFirstRender(); // Set firstRender to false after first render
+  }, [index, transRef]);
 
 
   return (
