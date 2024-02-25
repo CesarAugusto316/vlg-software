4947488423b651db0c26b0f74e9d555dc71a3232
cesaraugusto316 <cesaraugusto316@protonmail.com>
2024-02-24 20:37:37 -0500

    ✨ Update CreateCompanyForm and Modal components

--- src/app/companies/CreateCompanyForm.tsx
+++ src/app/companies/CreateCompanyForm.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 
 export const CreateCompanyStepForm: FC = () => {
   return (
-    <div className="bg-white p-6 rounded-lg">
+    <div className="bg-white p-6 rounded-lg w-[900px] h-[calc(100vh-100px)]">
       <h1>Hello World</h1>
     </div>
   );
--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,40 +1,45 @@
-import { useTransition, animated } from '@react-spring/web';
+import { useTransition, animated, UseTransitionProps } from '@react-spring/web';
 import { FC } from 'react';
 import { createPortal } from 'react-dom';
+
+
+const DEFAULT_TRANSITION_CONFIG = {
+  from: { y: '100%', opacity: 0 },
+  enter: { y: '0%', opacity: 1 },
+  leave: { y: '140%', opacity: 0 },
+  config: { tension: 170, mass: 1.7 }
+};
 
 
 interface ModalProps {
   isOpen: boolean;
   onClose: () => void;
   children: React.ReactNode;
+  transitionConfig?: UseTransitionProps
 }
 
-export const Modal: FC<ModalProps> = ({ isOpen, onClose, children }) => {
+export const Modal: FC<ModalProps> =
+  ({ isOpen, onClose, children, transitionConfig = DEFAULT_TRANSITION_CONFIG }) => {
 
-  const transition = useTransition(isOpen, {
-    from: { y: '200%', opacity: 0 },
-    enter: { y: '0%', opacity: 1 },
-    leave: { y: '200%', opacity: 0 },
-    config: { tension: 260, mass: 2 }
-  });
+    const transition = useTransition(isOpen, transitionConfig);
 
-  const AnimatedModal: FC = () => transition((style, isOpen) => (
-    isOpen && (
-      <animated.div
-        onClick={onClose}
-        style={{ opacity: style.opacity }}
-        className="backdrop-blur-[1px] bg-black/55 fixed w-screen h-screen z-50 flex items-center justify-center">
+    const AnimatedModal: FC = () => transition((style, isOpen) => (
+      isOpen && (
         <animated.div
-          onClick={(e) => e.stopPropagation()}
-          style={{ transform: style.y.to(y => `translateY(${y})`) }}
-        >
-          {children}
+          onClick={onClose}
+          style={{ opacity: style.opacity }}
+          className="backdrop-blur-[1px] bg-black/60 fixed w-screen h-screen z-50 flex items-center justify-center">
+          <animated.div
+            onClick={(e) => e.stopPropagation()}
+            style={{ transform: style.y.to(y => `translateY(${y})`) }}
+          >
+            {children}
+          </animated.div>
         </animated.div>
-      </animated.div>
-    )
-  ));
+      )
+    ));
 
-  return (
-    createPortal(<AnimatedModal />, document.getElementById('modal-root')!)
-  );
-};
+    return (
+      createPortal(<AnimatedModal />, document.getElementById('modal-root')!)
+    );
+  };
